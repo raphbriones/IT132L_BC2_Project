455d472a78a5fad47679604f9eadf43464d583a0
RenZeo <renzellateo11@gmail.com>
2017-09-08 13:32:03 +0800

    functional na yung add,remiove,update at patient button sa schedule though function is still incomplete dahil wala pa databate

---
+++ Dental Clinic System/Dental Clinic System/Remove Schedule.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Dental_Clinic_System
+{
+    partial class Remove_Schedule
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PatientName = new System.Windows.Forms.ComboBox();
+            this.Date = new System.Windows.Forms.ComboBox();
+            this.Time = new System.Windows.Forms.ComboBox();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.ConfirmBtn = new System.Windows.Forms.Button();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Patient Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Time";
+            // 
+            // PatientName
+            // 
+            this.PatientName.FormattingEnabled = true;
+            this.PatientName.Location = new System.Drawing.Point(102, 13);
+            this.PatientName.Name = "PatientName";
+            this.PatientName.Size = new System.Drawing.Size(170, 21);
+            this.PatientName.TabIndex = 3;
+            // 
+            // Date
+            // 
+            this.Date.FormattingEnabled = true;
+            this.Date.Location = new System.Drawing.Point(102, 43);
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(170, 21);
+            this.Date.TabIndex = 4;
+            // 
+            // Time
+            // 
+            this.Time.FormattingEnabled = true;
+            this.Time.Location = new System.Drawing.Point(102, 72);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(170, 21);
+            this.Time.TabIndex = 5;
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.Location = new System.Drawing.Point(16, 122);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.CancelBtn.TabIndex = 6;
+            this.CancelBtn.Text = "Cancel";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
+            // 
+            // ConfirmBtn
+            // 
+            this.ConfirmBtn.Location = new System.Drawing.Point(197, 122);
+            this.ConfirmBtn.Name = "ConfirmBtn";
+            this.ConfirmBtn.Size = new System.Drawing.Size(75, 23);
+            this.ConfirmBtn.TabIndex = 7;
+            this.ConfirmBtn.Text = "Confirm";
+            this.ConfirmBtn.UseVisualStyleBackColor = true;
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.Location = new System.Drawing.Point(107, 122);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(75, 23);
+            this.ClearBtn.TabIndex = 8;
+            this.ClearBtn.Text = "Clear";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // Remove_Schedule
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 157);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.ConfirmBtn);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.Time);
+            this.Controls.Add(this.Date);
+            this.Controls.Add(this.PatientName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Remove_Schedule";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Remove_Schedule";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox PatientName;
+        private System.Windows.Forms.ComboBox Date;
+        private System.Windows.Forms.ComboBox Time;
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.Button ConfirmBtn;
+        private System.Windows.Forms.Button ClearBtn;
+    }
+}
